5d80f06d0f8eafc0ad508cc7cd8f906f062f6463
Willianfreitas01 <willian.chris.89@gmail.com>
2024-01-25 16:04:52 -0300

    adicionando mais 2 camadas

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ import TileLayer from 'ol/layer/Tile.js';
 import TileWMS from 'ol/source/TileWMS.js';
 import View from 'ol/View.js';
 
+function addWMSTileLayer(url, layers) {
+  return new TileLayer({
+    source: new TileWMS({
+      attributions: '@geoserver',
+      url: url,
+      params: {
+        'LAYERS': layers,
+      },
+    }),
+  });
+}
 
 const map = new Map({
   target: 'map',
@@ -12,23 +23,12 @@ const map = new Map({
     new TileLayer({
       source: new OSM(),
     }),
+    addWMSTileLayer('http://localhost:8080/geoserver/portao/wms', 'portao:portao_g_logradouro'),
+    addWMSTileLayer('http://localhost:8080/geoserver/portao/wms', 'portao:portao_g_limite'),
+    addWMSTileLayer('http://localhost:8080/geoserver/portao/wms', 'portao:portao_g_bairro'),
   ],
   view: new View({
     center: [0, 0],
     zoom: 2,
   }),
 });
-
-
-
-const layer = new TileLayer({
-      source: new TileWMS ({
-        attribuitions: '@geoserver',
-        url:'http://localhost:8080/geoserver/portao/wms?',
-        params:{
-          'LAYERS': 'portao:portao_g_bairro',
-      }
-    })
-})
-
-map.addLayer(layer)
