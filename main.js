d195b09e5ce77016022ece4207235127a2128826
Willianfreitas01 <willian.chris.89@gmail.com>
2024-01-30 14:13:26 -0300

    refact(main): fazendo funções mais flexíveis e editando a centralização

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 import './style.css';
+import View from 'ol/View.js';
+import "ol/ol.css";
+import "ol-ext/dist/ol-ext.css";
 import Map from 'ol/Map.js';
 import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import TileWMS from 'ol/source/TileWMS.js';
-import View from 'ol/View.js';import "ol/ol.css"
-import "ol-ext/dist/ol-ext.css"
-
 import LayerSwitcher from "ol-ext/control/LayerSwitcher"
+import {transform} from 'ol/proj.js';
 
-function addWMSTileLayer(url, layers) {
+
+function addWMSTileLayer(title, url, layers) {
   return new TileLayer({
+    title:title,
     source: new TileWMS({
       attributions: '@geoserver',
       url: url,
@@ -20,19 +23,23 @@ function addWMSTileLayer(url, layers) {
   });
 }
 
+
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
+      title:'Mapa OL',
       source: new OSM(),
     }),
-    addWMSTileLayer('http://localhost:8080/geoserver/portao/wms', 'portao:portao_g_logradouro'),
-    addWMSTileLayer('http://localhost:8080/geoserver/portao/wms', 'portao:portao_g_limite'),
-    addWMSTileLayer('http://localhost:8080/geoserver/portao/wms', 'portao:portao_g_bairro'),
+
+    addWMSTileLayer('Logradouro','http://localhost:8080/geoserver/portao/wms', 'portao:portao_g_logradouro'),
+    addWMSTileLayer('Limite','http://localhost:8080/geoserver/portao/wms', 'portao:portao_g_limite'),
+    addWMSTileLayer('Bairro','http://localhost:8080/geoserver/portao/wms', 'portao:portao_g_bairro'),
   ],
+
   view: new View({
-    center: [0,0],
-    zoom: 2,
+    center: transform([-51.2902375592119384,-29.7268898592075494], 'EPSG:4326', 'EPSG:3857'),
+    zoom: 12,
   }),
 });
 
